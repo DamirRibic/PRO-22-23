8a6be381a6bebc24b536e3f25652f1111ed3f364
student <student@B23-16>
2022-10-05 08:26:05 +0200

    Add project files.

---
+++ Tower of Hanoi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tower_of_Hanoi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        } 
+        static void Premakni (int n,string začetni, string končni, string pomožni)
+        {
+            //prestavi n obročev iz začetnega na končni
+            if (n==1)
+            {
+                Console.WriteLine("Iz "+začetni+" na "+končni);
+                return;
+            }
+            Premakni(n - 1, začetni, pomožni, končni);
+            Console.WriteLine("Iz " + začetni + " na " + končni);
+            Premakni(n - 1, pomožni, končni, začetni);
+        }
+    }
+}
